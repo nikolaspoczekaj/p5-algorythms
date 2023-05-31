ea7ef7f8fc56ece73c1cf1dc4ee4fbef91eb0bc6
Nikolas Poczekaj <nikolas.poczekaj@gmail.com>
2023-05-31 16:36:34 +0200

    time slider

--- gameoflife.js
+++ gameoflife.js
@@ -1,83 +1,91 @@
-const TIMEBETWEENGENS = 100;
+
 const ROWS = 50;
 const COLS = 80;
 var grid;
 var next;
+var timeSlider;
+var divTime;
 
 function setup(){
+
+  
+
   createCanvas(800,500);
   background(240);
   noLoop();
   
+  createDiv('Time between Generations:');
+  timeSlider = createSlider(0,5,0.05,0.01);
+  divTime = createDiv('s');
 
   grid = makeGrid();
   grid = fillGridRandom(grid);
 }
 
 function draw(){
-  
-  
-    clear();
-    line(1,1,width,1);
-    line(1,1,1,height);
-    line(1,height-1,width-1,height-1);
-    line(width-1,1,width-1,height-1);
+  clear();
 
-    for (var x = 0; x < width; x += width / COLS) {
-      for (var y = 0; y < height; y += height / ROWS) {
-        stroke(200);
-        strokeWeight(1);
-        line(x, 0, x, height);
-        line(0, y, width, y);
+  divTime.html(timeSlider.value());
+  line(1,1,width,1);
+  line(1,1,1,height);
+  line(1,height-1,width-1,height-1);
+  line(width-1,1,width-1,height-1);
+
+  for (var x = 0; x < width; x += width / COLS) {
+    for (var y = 0; y < height; y += height / ROWS) {
+      stroke(200);
+      strokeWeight(1);
+      line(x, 0, x, height);
+      line(0, y, width, y);
+    }
+  }
+
+  for ( let i=0; i < ROWS; i++ ) {
+    for ( let j=0; j < COLS; j++ ) {
+
+      if(grid[i][j] === 1){
+        fill(51, 204, 51);
+        point((j*(height/ROWS)+(height/ROWS/2)), (i*(width/COLS)+(width/COLS/2)));
+        stroke('green');
+        strokeWeight(7);
       }
     }
+  }
 
-    for ( let i=0; i < ROWS; i++ ) {
-      for ( let j=0; j < COLS; j++ ) {
+  next = makeGrid();
 
-        if(grid[i][j] === 1){
-          fill(51, 204, 51);
-          point((j*(height/ROWS)+(height/ROWS/2)), (i*(width/COLS)+(width/COLS/2)));
-          stroke('green');
-          strokeWeight(7);
-        }
-      }
-    }
-
-    next = makeGrid();
-
-    for ( let i=0; i < ROWS; i++ ) {
-      for ( let j=0; j < COLS; j++ ) {
+  for ( let i=0; i < ROWS; i++ ) {
+    for ( let j=0; j < COLS; j++ ) {
         
-        let neighbours = countNeighbours(grid,i,j);
+      let neighbours = countNeighbours(grid,i,j);
 
-        if(grid[i][j] === 0){
-          if(neighbours === 3){
-            next[i][j] = 1;
-          }else{
-            next[i][j] = 0;
-          }
+      if(grid[i][j] === 0){
+        if(neighbours === 3){
+          next[i][j] = 1;
+        }else{
+          next[i][j] = 0;
         }
-
-        if(grid[i][j] === 1){
-          if(neighbours < 2){
-            next[i][j] = 0;
-          }else if(neighbours === 2 || neighbours === 3){
-            next[i][j] = 1;
-          }else if(neighbours > 3){
-            next[i][j] = 0;
-          }
-        }
-
       }
-    }
 
-    grid = next;
+      if(grid[i][j] === 1){
+        if(neighbours < 2){
+          next[i][j] = 0;
+        }else if(neighbours === 2 || neighbours === 3){
+          next[i][j] = 1;
+        }else if(neighbours > 3){
+          next[i][j] = 0;
+        }
+      }
+
+    }
+  }
+
+  grid = next;
   
   
-    setTimeout(function() {
-      redraw();
-    }, TIMEBETWEENGENS);
+  setTimeout(function() {
+    redraw();
+  }, timeSlider.value() * 1000);
   
 }
 
